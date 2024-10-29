24fb9ea63afa53fb60985c48728ff229654df1ef
gega109 <bakuravar1@gmail.com>
2024-10-29 20:27:41 +0400

    hm 18

--- script.js
+++ script.js
@@ -5,42 +5,35 @@ function TicketPrice(num) {
   switch (true) {
     case age < 10:
       return num + 5;
-      break;
     case age >= 10 && age <= 65:
       return num + 15;
-      break;
     case age > 65:
       return num + 10;
-      break;
     default:
       break;
   }
 }
-function typefunc(num) {
+function TypeFunc(num) {
   switch (true) {
     case movietype === "3Dfile":
       return num + 5;
-      break;
     case movietype === "regular":
       return num + 5;
-      break;
     default:
       break;
   }
 }
-function timefunc(num) {
+function TimeFunc(num) {
   switch (true) {
     case time === "matine":
       return num + 5;
-      break;
     case time === "evening":
       return num + 5;
-      break;
     default:
       break;
   }
 }
-console.log(TicketPrice(0));
-console.log(typefunc(0));
-console.log(timefunc(0));
-
+// console.log(TicketPrice(0));
+// console.log(TypeFunc(0));
+// console.log(TimeFunc(0));
+console.log(TicketPrice(0),TypeFunc(0),TimeFunc(0));
